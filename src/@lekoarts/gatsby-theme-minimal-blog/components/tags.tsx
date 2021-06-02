ed740193c8e493fd693f9dfabe82bb97e84af72f
Snehal Seth <sethsnehal95@gmail.com>
2021-06-03 02:24:41 +0530

    updates homepage by adding tags to it, tags page component changes, updates footer to remove theme from text

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/tags.tsx
@@ -0,0 +1,102 @@
+/** @jsx jsx */
+import Layout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout';
+import SEO from '@lekoarts/gatsby-theme-minimal-blog/src/components/seo';
+import useMinimalBlogConfig from '@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config';
+import replaceSlashes from '@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes';
+import { Flex, Grid } from '@theme-ui/components';
+import { graphql, Link, StaticQuery } from 'gatsby';
+import kebabCase from 'lodash.kebabcase';
+import React from 'react';
+import { Heading, jsx, Link as TLink } from 'theme-ui';
+
+type PostsProps = {
+  list?: {
+    fieldValue: string;
+    totalCount: number;
+  }[];
+  withLayout: boolean;
+};
+
+const Tags = ({ list = [], withLayout = true }: PostsProps) => {
+  const { tagsPath, basePath } = useMinimalBlogConfig();
+
+  if (withLayout) {
+    return (
+      <Layout>
+        <SEO title="Tags" />
+        <Heading as="h1" variant="styles.h1">
+          Tags
+        </Heading>
+        <Grid columns={[3]}>
+          {list.map((listItem) => (
+            <Flex
+              key={listItem.fieldValue}
+              mb={[1, 1, 2]}
+              sx={{ alignItems: `center` }}
+            >
+              <TLink
+                as={Link}
+                sx={{ variant: `links.listItem`, mr: 2 }}
+                to={replaceSlashes(
+                  `/${basePath}/${tagsPath}/${kebabCase(listItem.fieldValue)}`
+                )}
+              >
+                {listItem.fieldValue}{' '}
+                <span sx={{ color: `secondary` }}>({listItem.totalCount})</span>
+              </TLink>
+            </Flex>
+          ))}
+        </Grid>
+      </Layout>
+    );
+  } else {
+    return (
+      <StaticQuery
+        query={graphql`
+          query {
+            allPost(sort: { fields: tags___name, order: DESC }) {
+              group(field: tags___name) {
+                fieldValue
+                totalCount
+              }
+            }
+          }
+        `}
+        render={(data) => (
+          <React.Fragment>
+            <Heading as="h1" variant="styles.h1">
+              Tags
+            </Heading>
+            <br />
+            <Grid columns={[3]} gap="0.5rem">
+              {(data.allPost.group ?? []).map((listItem) => (
+                <Flex
+                  key={listItem.fieldValue}
+                  mb={[1, 1, 2]}
+                  sx={{ alignItems: `center` }}
+                >
+                  <TLink
+                    as={Link}
+                    sx={{ variant: `links.listItem`, mr: 2 }}
+                    to={replaceSlashes(
+                      `/${basePath}/${tagsPath}/${kebabCase(
+                        listItem.fieldValue
+                      )}`
+                    )}
+                  >
+                    {listItem.fieldValue}{' '}
+                    <span sx={{ color: `secondary` }}>
+                      ({listItem.totalCount})
+                    </span>
+                  </TLink>
+                </Flex>
+              ))}
+            </Grid>
+          </React.Fragment>
+        )}
+      />
+    );
+  }
+};
+
+export default Tags;
